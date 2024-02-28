64217ea2b813c3cdbc4556a71ab01a4d12163237
davidmolgard <molgarddavid@gmail.com>
2024-02-27 17:22:18 -0700

    allowed image to dynamically change

--- smashnotes/notes.js
+++ smashnotes/notes.js
@@ -1,1 +1,31 @@
-document.getElementById("username").textContent = localStorage.getItem("username");
+document.getElementById("username").textContent = getUsername();
+updateCharacterImage();
+
+function updateCharacterImage() {
+    var character = getNotesCharacter();
+
+    //base URL and an image ID
+    var baseURL = "https://ultimateframedata.com/characterart/";
+    var imageID = "notesCharacter";
+
+    // Construct the full image URL with a timestamp
+    var imageURL = baseURL + character + ".webp";
+
+    // Get the image element by ID and set the source
+    var myMainImage = document.getElementById(imageID);
+    if (myMainImage) {
+        myMainImage.src = imageURL;
+    }
+}
+
+function updateNotes() {
+    
+}
+
+function getNotesCharacter() {
+    return localStorage.getItem("notesCharacter");
+}
+
+function getUsername() {
+    return localStorage.getItem("username");
+}
